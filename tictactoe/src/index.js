d9ea38ca4a60edf3c0ffd42dc9f17070e6cc58a6
remnantsofred <remnantsofred@gmail.com>
2022-11-30 15:12:48 -0800

    Start setup TTT

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -1,6 +1,9 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view") // require appropriate file
+const Game = require("../ttt_node/game") // require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const figure = document.getElementById("ttt");
 });
+
+const playGame = new Game();
+
--- tictactoe/src/ttt-view.js
+++ tictactoe/src/ttt-view.js
@@ -1,7 +1,22 @@
 class View {
   constructor(game, el) {}
+  // when you construct a game, pass in the game and el you grabbed in the index.js file
+  setupBoard(el) {
+    let output = [];
+    // let ul = document.createElement('ul');
+    for(let i = 0; i < 3; i++){
+      let subarr = [];
+      let li = document.createElement('li');
 
-  setupBoard() {}
+      for(let j = i+1; j < 3-i; j++){
+        subarr.push(el[j][i])
+        length.datasomething = [i,j];
+        ul.appendChild(li);
+      }
+      output.push(subarr)
+    }
+    
+  }
   
   bindEvents() {}
 
